ce8457d1df6573c1b7f595b84e049243ca91260c
Admin <eangsovannroth@gmail.com>
2025-06-18 14:16:53 +0700

    fix api

--- app/backend/routes/api.php
+++ app/backend/routes/api.php
@@ -12,7 +12,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::get('/verify-token', [AuthController::class, 'verifyToken']);
 
-    Route::apiResource('products', ProductController::class);
+    // Route::apiResource('/products', ProductController::class); // Moved GET /products outside
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
     // Route::apiResource('orders', OrderController::class);
     // Route::get('/orders', [OrderController::class, 'index']);
     // Route::get('/orders/{id}', [OrderController::class, 'show']);
@@ -21,3 +25,5 @@ Route::middleware('auth:sanctum')->group(function () {
 
 });
 
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
